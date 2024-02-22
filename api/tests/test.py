4c96ba966ad56b217692331c97c85d76e867a7dd
Rob Ratcliffe <rlr@tcliffe.dev>
2024-02-22 18:58:35 -0500

    add failing django test to test failing pipeline

---
+++ api/tests/test.py
@@ -0,0 +1,12 @@
+from django.test import TestCase
+
+
+class SampleTest(TestCase):
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+    def test_sample(self):
+        assert False
